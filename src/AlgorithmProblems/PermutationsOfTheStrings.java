e1342726515b3144d76ef6e3a14d0dc9a9964458
MohanRaj <mraj86698@gmail.com>
2022-07-24 18:12:34 +0530

    Added Permutation of the String

---
+++ src/AlgorithmProblems/PermutationsOfTheStrings.java
@@ -0,0 +1,102 @@
+package AlgorithmProblems;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+
+public class PermutationsOfTheStrings {
+	static ArrayList<String> array = new ArrayList<>();
+	static ArrayList<String> array1 = new ArrayList<String>();
+
+	/**
+	 * Method to swap a string
+	 *
+	 * @param a String
+	 * @param i index of string
+	 * @param j index of string
+	 * @return swapped string
+	 */
+	public static String swapString(String a, int i, int j) {
+		char[] b = a.toCharArray();
+		char ch;
+		ch = b[i];
+		b[i] = b[j];
+		b[j] = ch;
+		return String.valueOf(b);
+	}
+
+	/**
+	 * Method to generate permutations using recursion
+	 *
+	 * @param str
+	 * @param start
+	 * @param end
+	 */
+	public static void generatePermutation(String str, int start, int end) {
+		// Prints the permutations
+		if (start == end - 1) {
+			array.add(str);
+		} else {
+			for (int i = start; i < end; i++) {
+				// Swapping the string by fixing a character
+				str = swapString(str, start, i);
+				// Recursively calling function generatePermutation() for rest of the characters
+				generatePermutation(str, start + 1, end);
+				// Backtracking and swapping the characters again.
+				str = swapString(str, start, i);
+			}
+		}
+	}
+
+	/**
+	 * method to generate permutations using iterative method
+	 *
+	 * @param s string
+	 */
+	public static void permutations(String s) {
+		/*string valueOf() method converts different types of values into string.*/
+
+		array1.add(String.valueOf(s.charAt(0)));
+
+		for (int i = 1; i < s.length(); i++) {
+
+			for (int j = array1.size() - 1; j >= 0; j--) {
+				String str = array1.remove(j);
+
+				for (int k = 0; k <= str.length(); k++) {
+					array1.add(str.substring(0, k) + s.charAt(i) + str.substring(k));
+				}
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("Enter a String");
+			String name = Utility.inputStringLine();
+			Utility.sc.close();
+			permutations(name);
+			generatePermutation(name, 0, name.length());
+			Collections.sort(array1);
+			Collections.sort(array);
+			int i;
+			for (i=0; i < array1.size(); i++) {
+				System.out.println(array1.get(i)  + " " + array1.get(i));
+			}
+
+			if (array1.equals(array)) {
+				System.out.println("Both are Same" + "\n The number of permutations are " + i);
+			} else {
+				System.out.println("wrong");
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+}
+
+
