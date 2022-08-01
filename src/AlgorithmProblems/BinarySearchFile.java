2996a032137d782f2a274380d932b7401169d759
MohanRaj <mraj86698@gmail.com>
2022-08-01 16:51:38 +0530

    Added Binary Search File

--- src/AlgorithmProblems/BinarySearchFile.java
+++ src/AlgorithmProblems/BinarySearchFile.java
@@ -1,78 +1,59 @@
 package AlgorithmProblems;
 /**
- * Program to search word from file
+ * This is an Binary Search File
+ * @author mraj
+ *
  */
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
+public class BinarySearchFile {
 
-	public class BinarySearchFile {
-		public static int binarySearch(String[] arr, String str){
-			int high = arr.length - 1, low = 0, mid;
-			Arrays.sort(arr);
-			while (low <= high) {
-				mid = (high + low) / 2;
-				if (str.equalsIgnoreCase(arr[mid])) {
-					return mid;
-				} else if (arr[mid].compareToIgnoreCase(str) < 0) {
-					low = mid + 1;
-				} else {
-					high = mid - 1;
-				}
-			}
-
-			return -1;
+	public void binarySearch(String[] array, int low, int high, String data) {
+		int mid = (low + high) / 2;
+		if (data.equals(array[mid])) {
+			System.out.println(data + " word is found ");
+			return;
 		}
 
-		public static String[] fileRead(File file) throws IOException {
-			String word = "";
-			ArrayList<String> list = new ArrayList<String>();
-			FileReader fr = new FileReader(file);
-			int content;
-			while ((content = fr.read()) != -1) {
-				if (content == 32) {
-					list.add(word);
-					word = "";
-				} else {
-					word = word + String.valueOf((char) content);
-				}
-			}
-			fr.close();
-			String[] s = new String[list.size()];
-			list.toArray(s);
-			return s;
+		if (mid == low || mid == high) {
+			System.out.println(data + " word can't found  ");
+			return;
 		}
 
-	public static void main(String[] args) throws IOException {
-
-		try {
-			File file = new File("C:\\Users\\mraj\\mohan\\BinarySearch.txt");
-			String arr[] = fileRead(file);
-			for (String s : arr) {
-				System.out.println(s);
-			}
-			while(true) {
-			System.out.println("\nEnter word to search");
-			String str = Utility.inputStringLine();
-			int n = binarySearch(arr, str);
-			if (n > 0) {
-				System.out.println("found");
-			} else {
-				System.out.println("not found");
-			}
-			}
-
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-
+		else if (data.compareTo(array[mid]) > 0)
+			binarySearch(array, mid, high, data);
+		else
+			binarySearch(array, low, mid, data);
 	}
 
+	public static void main(String[] args) {
 
+		String sentence = "hi huys whats up";
+		/**
+		 * Divides the String and return the array of SubStrings.
+		 */
+
+		String[] arrayOfString = sentence.toLowerCase().split("");
+
+		BinarySearchFile binary = new BinarySearchFile();
+
+		for (int i = 0; i < arrayOfString.length - 1; i++) {
+			for (int j = i + 1; j < arrayOfString.length; j++) {
+				if (arrayOfString[i].compareTo(arrayOfString[j]) > 0) {
+
+					String temp = arrayOfString[i];
+
+					arrayOfString[i] = arrayOfString[j];
+
+					arrayOfString[j] = temp;
+				}
+			}
+		}
+
+		System.out.println("Enter word to search");
+
+		String word = Utility.inputString();
+
+		binary.binarySearch(arrayOfString, 0, arrayOfString.length, word);
 	}
 
-
-
+}
